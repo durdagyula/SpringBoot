65ffb82ac61cb3c5ae93cd64076f54c73753f97a
Gyula <durdagyula@hotmail.com>
2016-11-26 20:05:57 +0100

    SpringBoot Devtools added

--- src/main/java/unimiskolc/springboot/ViewController.java
+++ src/main/java/unimiskolc/springboot/ViewController.java
@@ -1,5 +1,8 @@
 package unimiskolc.springboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +12,19 @@ import java.util.Date;
 @Controller
 public class ViewController {
 
+    //@Value("${app-mode}") cunstructor injection jobb!
+    private String appMode;
+
+    @Autowired
+    public ViewController(Environment environment){
+        appMode = environment.getProperty("app-mode");
+    }
+
     @RequestMapping("/")
     public String index(Model model){
         model.addAttribute("datetime", new Date());
         model.addAttribute("username","Durda Gyula");
-        model.addAttribute("mode", "development");
+        model.addAttribute("mode", appMode);
 
         return "index";
     }
