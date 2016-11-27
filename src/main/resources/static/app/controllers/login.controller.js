1d7de1c97450d75bb7c2537352a25ade4b1c7f08
Gyula <durdagyula@hotmail.com>
2016-11-27 15:07:04 +0100

    Register user added

--- src/main/resources/static/app/controllers/login.controller.js
+++ src/main/resources/static/app/controllers/login.controller.js
@@ -1,10 +1,26 @@
 (function () {
     'use strict';
 
-    angular.module('app').controller('LoginController',function ($scope, LoginService) {
+    angular.module('app').controller('LoginController',function ($http, $scope, LoginService) {
         $scope.msgtext = '';
+        $scope.msgsuccess = '';
+        $scope.msgerror = '';
         $scope.login = function (user) {
            LoginService.login(user, $scope);
+        }
+
+        $scope.register = function (newUser) {
+            var url = "/users/register/" + newUser.name + '/' + newUser.credits + '/' + newUser.password;
+            var userPromise = $http.post(url)
+            $scope.msgsuccess = '';
+            $scope.msgerror = '';
+            userPromise.then(function (response) {
+                if(response.data){
+                    $scope.msgsuccess = 'User registered successfully!';
+                }else{
+                    $scope.msgerror = 'User name is reserved!';
+                }
+            })
         }
     });
 
